e543117e5326fa461d1925ad15f116f4c2cf79d9
Igor <snoozzzy7@gmail.com>
2022-11-04 16:54:22 +0100

    Added base component and page

---
+++ src/components/base/BaseComponent.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment, FunctionComponent, ReactElement } from 'react';
+
+
+const BaseComponent:FunctionComponent = ():ReactElement => {
+
+    return (
+        <Fragment>
+            <h1>Base Component</h1>
+        </Fragment>
+    );
+}
+
+export default BaseComponent;
---
+++ src/pages/base/BasePage.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent, ReactElement } from 'react';
+import BaseComponent from '../../components/base/BaseComponent';
+
+
+const BasePage:FunctionComponent = ():ReactElement => <BaseComponent />
+
+export default BasePage;
