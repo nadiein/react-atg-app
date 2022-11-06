c98a90bc38b831dc856146e67cf36823e9356030
Igor <snoozzzy7@gmail.com>
2022-11-06 23:51:17 +0100

    Updated base component to show product select

--- src/components/base/BaseComponent.tsx
+++ src/components/base/BaseComponent.tsx
@@ -1,11 +1,38 @@
-import React, { Fragment, FunctionComponent, ReactElement } from 'react';
+import React, { Fragment, FunctionComponent, ReactElement, useEffect, useState } from 'react';
+import { ProductType } from '../../models/product/ProductModel';
+import ProductsService from '../../services/products/ProductsService';
+import { REQUEST_OPTIONS, SearchUrlBuilder } from '../../utils/http/HttpHelpers';
+import OptionsFormControl from '../form-controls/OptionsFormControl';
+import Table from '../table/Table';
 
+
+const productOptions = [
+    { id: 0, name: ProductType.V75 },
+    { id: 1, name: ProductType.GS75 },
+    { id: 2, name: ProductType.V86 }
+]
 
 const BaseComponent:FunctionComponent = ():ReactElement => {
+    const [products, setProducts] = useState([]);
+    const [productType, setProductType] = useState(ProductType.V75);
+    const productRequest:string = SearchUrlBuilder.forProduct(productType).build();
 
+    REQUEST_OPTIONS.method = 'GET';
+    REQUEST_OPTIONS.url = productRequest;
+
+    const handleOnChange = (e:any):void => {
+        setProductType(e.target.value)
+    }
+
+    useEffect(() => {
+        ProductsService.getProducts(REQUEST_OPTIONS).then((res:any[]) => setProducts(res));
+    }, [productType]);
+    console.warn(products)
     return (
         <Fragment>
             <h1>Base Component</h1>
+            <OptionsFormControl options={ productOptions } getSelectOnChange={ handleOnChange } />
+            <Table />
         </Fragment>
     );
 }
