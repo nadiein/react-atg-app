c85857e302d1933a00adff9a66188a309cc9e0c6
Igor <snoozzzy7@gmail.com>
2022-11-07 13:42:40 +0100

    Added trainer model

---
+++ src/models/trainer/TrainerModel.ts
@@ -0,0 +1,10 @@
+import DriverDetailsModel from '../driver/DriverModel';
+
+
+class TrainerDetailsModel {
+    public firstName:string;
+    public lastName:string;
+    public driver:DriverDetailsModel;
+}
+
+export default TrainerDetailsModel;
