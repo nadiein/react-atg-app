cc13af3116f207665e7585ca26b7f3da8c17db1d
Igor <snoozzzy7@gmail.com>
2022-11-07 21:22:01 +0100

    Fetch product and games in parent component

--- src/components/base/BaseComponent.tsx
+++ src/components/base/BaseComponent.tsx
@@ -1,5 +1,7 @@
 import React, { Fragment, FunctionComponent, ReactElement, useEffect, useState } from 'react';
+import { GameModel } from '../../models/game/GameModel';
 import { ProductModel, ProductType } from '../../models/product/ProductModel';
+import GamesService from '../../services/games/GamesService';
 import ProductsService from '../../services/products/ProductsService';
 import { REQUEST_OPTIONS, SearchUrlBuilder } from '../../utils/http/HttpHelpers';
 import OptionsFormControl from '../form-controls/OptionsFormControl';
@@ -13,26 +15,39 @@ const productOptions = [
 ]
 
 const BaseComponent:FunctionComponent = ():ReactElement => {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState(new ProductModel());
     const [productType, setProductType] = useState(ProductType.V75);
-    const productRequest:string = SearchUrlBuilder.forProduct(productType).build();
 
     REQUEST_OPTIONS.method = 'GET';
-    REQUEST_OPTIONS.url = productRequest;
 
     const handleOnChange = (e:any):void => {
         setProductType(e.target.value)
     }
 
+    const productRequest:string = SearchUrlBuilder.forProduct(productType).build();
+
     useEffect(() => {
+        REQUEST_OPTIONS.url = productRequest;
+
         ProductsService.getProducts(REQUEST_OPTIONS).then(( res:any ) => setProducts(res));
     }, [productType]);
+
+    useEffect(() => {
+        for (const details of products['productDetails']) {
+            const detailsId = details['id'];
+            const gameRequest:string = SearchUrlBuilder.forGames(detailsId).build();
+
+            REQUEST_OPTIONS.url = gameRequest;
+
+            GamesService.getGames(REQUEST_OPTIONS).then(( res:any ) => details['games'] = res);
+        }
+    }, [products]);
 
     return (
         <Fragment>
             <h1>Base Component</h1>
             <OptionsFormControl options={ productOptions } getSelectOnChange={ handleOnChange } />
-            <GameTable products={ products } />
+            <GameTable productModel={ products } />
         </Fragment>
     );
 }
