1a6070ba8d6deba48778efd26dd5a1668e220723
Igor <snoozzzy7@gmail.com>
2022-11-08 23:59:07 +0100

    Updated game details component view and logic

--- src/components/game-table/GameTableDetails.tsx
+++ src/components/game-table/GameTableDetails.tsx
@@ -1,16 +1,35 @@
 import React, { Fragment, FunctionComponent, ReactElement } from 'react';
+import { GameRacesModel } from '../../models/game/GameModel';
 import { ProductDetailsModel } from '../../models/product/ProductModel';
+import { formatTimeToTwoDigits } from '../../utils/date/DateTimeHelpers';
+import GameRaceDetails from './GameRaceDetails';
 
 
 type GameTableDetailsTypes = {
-    product:ProductDetailsModel
+    product:ProductDetailsModel,
+    gameRaces:GameRacesModel[]
 }
 
-const GameTableDetails:FunctionComponent<GameTableDetailsTypes> = ({ product }:GameTableDetailsTypes):ReactElement => {
+const GameTableDetails:FunctionComponent<GameTableDetailsTypes> = ({ product, gameRaces }:GameTableDetailsTypes):ReactElement => {
+    const { name } = product['track'];
+    const trackTime = formatTimeToTwoDigits(new Date(product['startTime']));
 
     return (
         <Fragment>
-            <h1>Game table details</h1>
+            <div className='table-row'>
+                <div className='table-header'>
+                    <h2>{ name } - { trackTime }</h2>
+                </div>
+                <div className='table-body'>
+                    {
+                        gameRaces ? gameRaces.map((race:GameRacesModel, index:number) => (
+                            <GameRaceDetails 
+                                gameRaces={ race }
+                                key={ index } />
+                        )) : 'No game races details'
+                    }
+                </div>
+            </div>
         </Fragment>
     );
 }
