ae06dc135ec02cde587b829a3a6264d75fa0ea87
Igor <snoozzzy7@gmail.com>
2022-11-08 23:59:31 +0100

    Updated game table component

--- src/components/game-table/GameTable.tsx
+++ src/components/game-table/GameTable.tsx
@@ -1,21 +1,30 @@
 import React, { Fragment, FunctionComponent, ReactElement } from 'react';
+import { GameModel } from '../../models/game/GameModel';
 import { ProductDetailsModel, ProductModel } from '../../models/product/ProductModel';
 import GameTableDetails from './GameTableDetails';
 
+import './game-table.scss';
+
 
 type GamesTableTypes = {
-    productModel:ProductModel
+    productModel:ProductModel,
+    gamesModel:GameModel
 }
 
-const GameTable:FunctionComponent<GamesTableTypes> = ({ productModel }:GamesTableTypes):ReactElement => {
+const GameTable:FunctionComponent<GamesTableTypes> = ({ productModel, gamesModel }:GamesTableTypes):ReactElement => {
+    const { gameRaces } = gamesModel;
 
     return (
         <Fragment>
             <div className='games-table-wrapper'>
                 {
-                    productModel['productDetails'] ? productModel['productDetails'].map((product:ProductDetailsModel, index:number) => (
-                        <GameTableDetails product={ product } key={ index } />
-                    )) : 'Have no Game Details'
+                    productModel['productDetails'] && productModel['productDetails'].length > 0 ? <div className='table-wrapper'>
+                        {
+                            productModel['productDetails'].map((product:ProductDetailsModel, index:number) => (
+                                <GameTableDetails product={ product } gameRaces={ gameRaces } key={ index } />
+                            ))
+                        }
+                    </div> : 'Have no Game Details'
                 }
             </div>
         </Fragment>
